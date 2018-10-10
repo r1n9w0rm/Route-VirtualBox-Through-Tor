601df5fecf36e665ed2179d5d73e07d1c8785a77
JesseBropez <34920476+JesseBropez@users.noreply.github.com>
2018-10-10 17:41:58 -0600

    Update middlebox.sh

--- middlebox.sh
+++ middlebox.sh
@@ -3,47 +3,32 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as sudo/root." 
    exit 1
 fi
-echo "This script will create a virtual interface and route all traffic going through it, through Tor."
+echo "This script will route all traffic from VirtualBox through Tor."
 read -p "Press enter to continue"
-echo y | apt-get install bridge-utils
-INT_IF="vboxnet0"
-echo "# VirtualBox NAT bridge" >> /etc/network/interfaces
-echo "auto " $INT_IF >> /etc/network/interfaces
-echo "iface " $INT_IF " inet static" >> /etc/network/interfaces
-echo "What IP address would you like as the gateway for the Virtual Adapter (i.e. 192.168.1.2)?"
-read ipaddr
-echo "address " $ipaddr >> /etc/network/interfaces
-echo "netmask 255.255.255.0" >> /etc/network/interfaces
-echo "bridge_ports none" >> /etc/network/interfaces
-echo "bridge_maxwait 0" >> /etc/network/interfaces
-echo "bridge_fd 1" >> /etc/network/interfaces
-echo "What IP address would you like for post routing? (i.e. 192.168.1.0)?"
-read postroutipaddr
-echo "up iptables -t nat -I POSTROUTING -s $postroutipaddr/24 -j MASQUERADE" >> /etc/network/interfaces
-echo "down iptables -t nat -D POSTROUTING -s $postroutipaddr/24 -j MASQUERADE" >> /etc/network/interfaces
-ip link delete $INT_IF
-ifup $INT_IF
-echo y | apt-get install dnsmasq
-echo "interface=" $INT_IF >> /etc/dnsmasq.conf
-echo "What IP address would you like to start at for the DHCP range (i.e. 192.168.1.3)?"
-echo "dhcp-range=192.168.1.3,192.168.1.254,1h" >> /etc/dnsmasq.conf
-service /etc/init.d/dnsmasq restart
+
 echo y | apt-get install tor
-echo "VirtualAddrNetwork 10.192.0.0/10" >> /etc/tor/torrc
 echo "AutomapHostsOnResolve 1" >> /etc/tor/torrc
-echo "TransPort $ipaddr:9040" >> /etc/tor/torrc
-echo "DNSPort $ipaddr:53" >> /etc/tor/torrc
-service tor restart
-echo "What local ipaddress ranges would you like to avoid routing through tor (i.e. 10.8.0.0/10, x.x.x.x/x)?"
-read NON_TOR
+echo "What is the gateway tor will be routed through? i.e. 192.168.1.2"
+read GATEWAY
+vboxmanage hostonlyif create
+echo "What is the interface name for VirtualBox? i.e. vboxnet0"
+read INTERFACENAME
+vboxmanage hostonlyif ipconfig $INTERFACENAME --ip $GATEWAY
+echo "TransPort $GATEWAY:9050" >> /etc/tor/torrc
+echo "DNSPort $GATEWAY:1053" >> /etc/tor/torrc
+echo "What exit nodes would you like for tor? Example: {US},{GB}"
+read EXIT_NODES
+echo "ExitNodes "$EXIT_NODES >> /etc/tor/torrc
 echo "Flushing IP Tables..."
 iptables -F
 iptables -t nat -F
-iptables -t nat -I POSTROUTING -s $postroutipaddr/24 -j MASQUERADE
-for NET in $NON_TOR; do
- iptables -t nat -A PREROUTING -i $INT_IF -d $NET -j RETURN
-done
-iptables -t nat -A PREROUTING -i $INT_IF -p udp --dport 53 -j REDIRECT --to-ports 53
-iptables -A FORWARD -i $INT_IF -p udp -j DROP
-iptables -t nat -A PREROUTING -i $INT_IF -p tcp --syn -j REDIRECT --to-ports 9040
-echo "Finished :) Make sure all VirtualBox machines have their adapters set to Host-only Adapter - vboxnet0"
+echo "Creating IP Tables rules..."
+iptables -t nat -A PREROUTING ! -d $GATEWAY/32 -i vboxnet0 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports 9050
+iptables -t nat -A PREROUTING ! -d $GATEWAY/32 -i vboxnet0 -p udp -m udp --dport 53 -j REDIRECT --to-ports 1053
+echo "Restarting tor..."
+service tor restart
+sed -i '3d' /etc/resolv.conf
+echo "What DNS would you like set in /etc/resolv.conf? i.e. 8.8.8.8"
+read NEWDNS
+echo "nameserver $NEWDNS" >> /etc/resolv.conf
+echo "Finished :) Make sure all VirtualBox machines have their adapters set to Host Only Adapter - vboxnet0"
